d2d36263643ca56ad885acc855c3a5bb29bd9d24
codefingas <udohezekiel34@yahoo.com>
2021-06-13 13:13:55 +0100

    udpate: worked on special variables

---
+++ specialvariables.sol
@@ -0,0 +1,23 @@
+//import solidity
+pragma solidity ^0.4.0;
+
+contract SimpleContract {
+    //block
+    block.number;
+    block.coinbase();
+    block.difficulty;
+
+    /**
+        comments go here
+        and here
+     */
+
+     //message
+     msg.sender;
+     msg.value;
+     msg.data;
+
+
+     //transaction
+     tx.origin
+}
