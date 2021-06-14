645a7aaf78679adf34005f1a054f806a36840ac2
codefingas <udohezekiel34@yahoo.com>
2021-06-14 22:08:14 +0100

    update: built base for contract operation

---
+++ simplecontract.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+contract Inheritance {
+    address owner;
+    bool deceased;
+    uint money;
+
+
+    constructor() public payable{
+        owner = msg.sender;
+        money = msg.value;
+        deceased = false;
+    }
+
+    address[] wallets;
+
+    mapping (address => uint) inheritance;
+}
