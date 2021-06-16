fc4089fd70626b96ef647d32fcace407b4415eb3
codefingas <udohezekiel34@yahoo.com>
2021-06-16 09:16:10 +0100

    feature: added Inheritance contract

--- simplecontract.sol
+++ simplecontract.sol
@@ -1,18 +1,43 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.8.0;
 
 contract Inheritance {
     address owner;
     bool deceased;
-    uint money;
+    uint256 money;
 
-
-    constructor() public payable{
+    constructor() public payable {
         owner = msg.sender;
         money = msg.value;
         deceased = false;
     }
 
+    modifier oneOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier isDeceased {
+        require(deceased == true);
+        _;
+    }
+
     address[] wallets;
 
-    mapping (address => uint) inheritance;
+    mapping(address => uint256) inheritance;
+
+    function setup(address _wallet, uint _inheritance) public oneOwner {
+        wallets.push(_wallet);
+        inheritance[_wallet] = _inheritance;
+    }
+
+    function moneyPaid() private isDeceased {
+        for(uint i = 0; i < wallets.length; i++){
+            payable(wallets[i]).transfer(inheritance[wallets[i]]);
+        }
+    }
+
+    function died() public oneOwner {
+        deceased = true;
+        moneyPaid();
+    }
 }
